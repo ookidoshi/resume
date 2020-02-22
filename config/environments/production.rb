ed0e0884ec2d58474a853081f5c5bdfb30ea3804
ookidoshi <okidookido@gmail.com>
2020-02-22 20:17:35 +0900

    fix
    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "resume_#{Rails.env}"
 
-  # config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = false
 
 
   # config.assets.initialize_on_precompile = false
